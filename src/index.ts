962a3d6e7a0f5d1faacef335506b05f07377040b
kelvin joseph <kelvinnjoseph77@gmail.com>
2026-01-02 11:51:14 +0300

    Define the auth routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import type { Request, Response, Express } from "express";
 import { PORT } from "./secrets.js";
+import rootRouter from "./routes/index.js";
 
 const app: Express = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working");
-});
+app.use(express.json());
+
+app.use('/api/v1', rootRouter)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
