eb0c89378d64a14fc7c47da516db97f93b370b8c
kelvin joseph <kelvinnjoseph77@gmail.com>
2026-01-02 10:20:55 +0300

    create folder structure for the app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import express from 'express';
+import express from "express";
+import type { Request, Response, Express } from "express";
 
-const app = express();
+const app: Express = express();
 
-app.get('/', (req, res) => {
-    res.send('Working');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Working");
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+  console.log("Server is running on port 3000");
+});
